7158c988b92ac4dfb5fd4487e5b6ac5da60c6f20
rashadg1030 <rashad.sasaki@gmail.com>
2018-09-20 01:10:27 -0700

    Added functionality for displaying game board in the console.

--- src/Main.hs
+++ src/Main.hs
@@ -16,3 +16,33 @@ startingBoard = [[Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing,
                  [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
                  [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing],
                  [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]]
+
+displayBoard :: Board -> IO ()
+displayBoard = putStr . (++) "\n-----------------\n" . boardString 
+
+
+boardString :: Board -> String
+boardString [] = "ERR: INVALID BOARD"
+boardString [x] = rowString x
+boardString (x:xs) = rowString x ++ boardString xs 
+
+rowString :: Row -> String
+rowString [] = "ERR: INVALID ROW"
+rowString [x] = cellString x ++ "|\n-----------------\n"
+rowString (x:xs) = cellString x ++ rowString xs
+
+cellString :: Cell -> String
+cellString (Nothing) = "| "
+cellString (Just B) = "|B"
+cellString (Just W) = "|W"
+
+
+placeDisc :: Disc -> Board -> (Char, Int) -> Board
+placeDisc x y (c, i) = undefined
+
+hasMoves :: Disc -> Board -> Bool
+hasMoves x = undefined
+
+flip :: Board -> Board
+flip = undefined
+
