407cde1ef319845a9cfc3d31709fc751a91195e6
rashadg1030 <rashad.sasaki@gmail.com>
2018-09-22 14:58:36 -0700

    Tried creating a test suite for testing the game. Need help with that. Organized the file structure of the project some more.

---
+++ tests/Test.hs
@@ -0,0 +1,8 @@
+module Test where
+
+import Main
+
+main :: IO ()
+main = undefined
+
+-- TODO: placeRow (Just B) 3 [Nothing, Nothing, Nothing, Just W, Just B, Nothing, Nothing, Nothing]
--- testsuite/tests/Test.hs
+++
@@ -1,5 +0,0 @@
-module Test where
-
-import Main
-
--- TODO: placeRow (Just B) 3 [Nothing, Nothing, Nothing, Just W, Just B, Nothing, Nothing, Nothing]
