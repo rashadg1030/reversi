f147949d560bbca52ecfa08665b6348accb70b66
rashadg1030 <rashad.sasaki@gmail.com>
2018-09-30 22:27:45 -0700

    Added testBoard3 for testing multi-direction captures. Will start splitting code into different modules/files so code is more readable and organized.

--- src/Main.hs
+++ src/Main.hs
@@ -62,6 +62,35 @@ testBoard1 = makeBoard [((3,3), White), ((4,3), White), ((5,3), White), ((3,4), 
 testBoard2 :: Board 
 testBoard2 = makeBoard [((3,3), White), ((4,3), White), ((3,4), Black), ((4,4), Black), ((5,4), Black), ((4,5), White), ((4,6), White)]
 
+testBoard3 :: Board
+testBoard3 = makeBoard [((2,4), White)
+                      , ((1,4), White)
+                      , ((0,4), Black)
+                      , ((4,4), White)
+                      , ((5,4), White)
+                      , ((6,4), White)
+                      , ((7,4), Black)
+                      , ((3,3), White)
+                      , ((3,2), White)
+                      , ((3,1), White)
+                      , ((3,0), Black)
+                      , ((3,5), White)
+                      , ((3,6), White)
+                      , ((3,7), Black)
+                      , ((4,3), White)
+                      , ((5,2), White)
+                      , ((6,1), White)
+                      , ((7,0), Black)
+                      , ((2,5), White)
+                      , ((1,6), White)
+                      , ((0,7), Black)
+                      , ((2,3), White)
+                      , ((1,2), White)
+                      , ((0,1), Black)
+                      , ((4,5), White)
+                      , ((5,6), White)
+                      , ((6,7), Black)] 
+
 makeBoard :: [(Location, Disc)] -> Board
 makeBoard = Map.fromList
 
