14dd3bab3179b0dca4ef66d8c1e72806871157ec
rashadg1030 <rashad.sasaki@gmail.com>
2018-10-26 16:59:32 -0700

    Fixed formatting.

--- src/Main.hs
+++ src/Main.hs
@@ -12,7 +12,7 @@ import Board
 import Types 
 
 main :: IO ()
-main =  do 
+main = do 
   let startingState = (State Black startingBoard)
   runGame startingState
 
@@ -23,25 +23,25 @@ runGame state@(State disc board) = forever $ do
 
   case possibleMoves disc board of
     [] -> do
-           putStrLn "#PASS#"
-           (return (State (flipDisc disc) board)) >>= runGame
+            putStrLn "#PASS#"
+            (return (State (flipDisc disc) board)) >>= runGame
     _  -> do
-           putStrLn $ moveMessage disc
-           input <- getLine
-            
-           case readMaybe input of
-            Nothing -> do
-                        putStrLn "Invalid input. Try Again."
-                        (return state) >>= runGame
-            (Just loc) -> do
-                           if (elem loc (possibleMoves disc board)) then
-                            do   
-                             putStrLn "Valid location."
-                             (return (State (flipDisc disc) (makeMove disc loc board))) >>= runGame
-                           else
-                            do 
-                             putStrLn "Can't make that move. Try Again."
-                             (return state) >>= runGame
+            putStrLn $ moveMessage disc
+            input <- getLine
+              
+            case readMaybe input of
+              Nothing    -> do
+                              putStrLn "Invalid input. Try Again."
+                              (return state) >>= runGame
+              (Just loc) -> do
+                              if (elem loc (possibleMoves disc board)) then
+                                do   
+                                  putStrLn "Valid location."
+                                  (return (State (flipDisc disc) (makeMove disc loc board))) >>= runGame
+                              else
+                                do 
+                                  putStrLn "Can't make that move. Try Again."
+                                  (return state) >>= runGame  
       
 moveMessage :: Disc -> String
 moveMessage disc = (discName disc) ++ "'s move. Enter a location in the format (x,y). Ctrl + C to quit."
@@ -52,8 +52,7 @@ discName Black = "Black"
 
 randomGame :: IO ()
 randomGame = do
-  let startingState = (State Black startingBoard)
-  genRandomGame startingState 
+  genRandomGame (State Black startingBoard) 
 
 genRandomGame :: State -> IO ()
 genRandomGame state@(State disc board) = forever $ do
@@ -73,23 +72,24 @@ genRandomGame state@(State disc board) = forever $ do
 
 genLoc :: State -> IO (Int, Int)
 genLoc state@(State disc board) = do
-                                   x <- randomRIO (0,7)
-                                   y <- randomRIO (0,7)
-                                   if elem (x, y) possible then
-                                    return (x,y)
-                                   else genLoc state
-                                where 
-                                  possible = possibleMoves disc board
+                                    x <- randomRIO (0,7)
+                                    y <- randomRIO (0,7)
+                                    if elem (x, y) possible then
+                                      return (x,y)
+                                    else genLoc state
+                                    where 
+                                      possible = possibleMoves disc board
 
 gameEnd :: State -> IO ()
 gameEnd state@(State disc board) = 
   if noMoves state then
-    do putBoard board
-       if (isWinner Black board) then
+    do 
+      putBoard board
+      if (isWinner Black board) then
         putStrLn "Black won! White lost!"
-       else putStrLn "White won! Black lost!"
-       putStrLn "Better luck next time!"
-       exitSuccess
+      else putStrLn "White won! Black lost!"
+        putStrLn "Better luck next time!"
+        exitSuccess
   else return () 
 
 noMoves :: State -> Bool
@@ -97,9 +97,9 @@ noMoves state@(State disc board) = ((length $ possibleMoves disc board) == 0) &&
 
 isWinner :: Disc -> Board -> Bool
 isWinner disc board = answer
-                    where
-                      step1 = Map.toList board 
-                      step2 = map snd step1
-                      step31 = filter (\d1 -> d1 == disc) step2
-                      step32 = filter (\d2 -> d2 == (flipDisc disc)) step2
-                      answer = (length step31) > (length step32) 
+where
+  step1 = Map.toList board 
+  step2 = map snd step1
+  step31 = filter (\d1 -> d1 == disc) step2
+  step32 = filter (\d2 -> d2 == (flipDisc disc)) step2
+  answer = (length step31) > (length step32) 
