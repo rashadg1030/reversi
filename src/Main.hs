6c65a6fa5b52e3ede51dc60c62e5f57cdf6b6354
rashadg1030 <rashad.sasaki@gmail.com>
2018-10-27 12:42:09 -0700

    Replaced if-else statements with case statements where necessary. Fixed some formatting issues.

--- src/Main.hs
+++ src/Main.hs
@@ -21,26 +21,27 @@ runGame state@(State disc board) = do
   putBoard board
 
   case possibleMoves disc board of
-    [] -> do
-            putStrLn "#PASS#"
-            runGame (State (flipDisc disc) board)
-    _  -> do
-            putStrLn $ moveMessage disc
-            input <- getLine
-              
-            case readMaybe input of
-              Nothing    -> do
-                              putStrLn "Invalid input. Try Again."
-                              runGame state
-              (Just loc) -> do
-                              if (elem loc (possibleMoves disc board)) then
-                                do   
-                                  putStrLn "Valid location."
-                                  runGame (State (flipDisc disc) (makeMove disc loc board))
-                              else
-                                do 
-                                  putStrLn "Can't make that move. Try Again."
-                                  runGame state  
+    [] ->     do
+                putStrLn "#PASS#"
+                runGame (State (flipDisc disc) board)
+    (x:xs) -> do
+                putStrLn $ moveMessage disc
+                input <- getLine
+                  
+                case readMaybe input of
+                  Nothing    -> do
+                                  putStrLn "Invalid input. Try Again."
+                                  runGame state
+                  (Just loc) -> do
+                                  let possible = possibleMoves disc board
+                                  if (elem loc possible) then
+                                    do   
+                                      putStrLn "Valid location."
+                                      runGame (State (flipDisc disc) (makeMove disc loc board))
+                                  else
+                                    do 
+                                      putStrLn "Can't make that move. Try Again."
+                                      runGame state  
       
 moveMessage :: Disc -> String
 moveMessage disc = (discName disc) ++ "'s move. Enter a location in the format (x,y). Ctrl + C to quit."
@@ -57,30 +58,27 @@ genRandomGame :: State -> IO ()
 genRandomGame state@(State disc board) = do
   gameEnd state
   putBoard board   
-  if (possibleMoves disc board == []) then
-    do
-      putStrLn "#PASS#"
-      let newState = (State (flipDisc disc) board)
-      genRandomGame newState 
-  else
-    do
-      print disc
-      loc <- genLoc state
-      putStr "Move: "
-      print loc
-      let newState = (State (flipDisc disc) (makeMove disc loc board))
-      genRandomGame newState 
+  case possibleMoves disc board of
+    []     -> do
+                putStrLn "#PASS#"
+                let newState = (State (flipDisc disc) board)
+                genRandomGame newState 
+    (x:xs) -> do
+                print disc
+                loc <- genLoc state
+                putStr "Move: "
+                print loc
+                let newState = (State (flipDisc disc) (makeMove disc loc board))
+                genRandomGame newState 
 
 genLoc :: State -> IO (Int, Int)
 genLoc state@(State disc board) = do
                                     x <- randomRIO (0,7)
                                     y <- randomRIO (0,7)
-                                    if elem (x, y) possible then
-                                      return (x,y)
-                                    else 
-                                      genLoc state
-                                    where 
-                                      possible = possibleMoves disc board
+                                    let possible = possibleMoves disc board
+                                    if elem (x, y) possible then return (x,y) else genLoc state
+                                     
+                                    
 
 gameEnd :: State -> IO ()
 gameEnd state@(State disc board) = 
