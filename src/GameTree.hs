118d2f574be167710004073f915690b35900ae3c
rashadg1030 <rashad.sasaki@gmail.com>
2019-03-27 18:33:22 -0700

    Moved comments.

--- src/GameTree.hs
+++ src/GameTree.hs
@@ -21,34 +21,6 @@ countNodes :: RoseTree a -> Int
 countNodes (Node _ []) = 1
 countNodes (Node _ xs) = 1 + (sum $ map countNodes xs) 
 
--- instance Semigroup (RoseTree a) where
---     --(<>) :: RoseTree a -> RoseTree a -> RoseTree a
---     (<>) (Node x rxs) y' = Node x (y':rxs)
---     --(<>) x' (Node y rys) = Node y (x':rys)
-
--- instance Foldable RoseTree where 
-
-
--- instance Applicative RoseTree where
---     pure :: a -> RoseTree a
---     pure x = Node x []
-    -- Maybe No Applicative??
-    -- (<*>) :: RoseTree (a -> b) -> RoseTree a -> RoseTree b
-    -- (<*>) (Node f []) (Node x _)  = Node (f x) []
-    -- (<*>) (Node f _) (Node x [])  = Node (f x) []
-    -- (<*>) (Node f fs) (Node x xs) = Node (f x) (listApply ((<*>) fs) xs)
-    -- fs :: [RoseTree (a -> b)]
-    -- xs :: [RoseTree a]
-    --     where 
-    --         answer :: RoseTree b
-    --         answer = (List.<*>) (fs <*>) xs    
-            
--- How to return [a] if emptyList of functions is passed in
--- listApply :: [(a -> b)] -> [a] -> [b]
--- listApply [] _  = []
--- listApply _  [] = []
--- listApply fs xs = [ f x | f <- fs, x <- xs] 
-
 gameStateToNode :: GameState -> RoseTree GameState
 gameStateToNode gs = Node gs []
 
@@ -78,6 +50,33 @@ testGenGameTree depth = genGameTree depth seed
 -- findBestMove d t = case t of
 --                     Node a [] -> getLoc 
 
+-- instance Semigroup (RoseTree a) where
+--     --(<>) :: RoseTree a -> RoseTree a -> RoseTree a
+--     (<>) (Node x rxs) y' = Node x (y':rxs)
+--     --(<>) x' (Node y rys) = Node y (x':rys)
+
+-- instance Foldable RoseTree where 
+
+
+-- instance Applicative RoseTree where
+--     pure :: a -> RoseTree a
+--     pure x = Node x []
+    -- Maybe No Applicative??
+    -- (<*>) :: RoseTree (a -> b) -> RoseTree a -> RoseTree b
+    -- (<*>) (Node f []) (Node x _)  = Node (f x) []
+    -- (<*>) (Node f _) (Node x [])  = Node (f x) []
+    -- (<*>) (Node f fs) (Node x xs) = Node (f x) (listApply ((<*>) fs) xs)
+    -- fs :: [RoseTree (a -> b)]
+    -- xs :: [RoseTree a]
+    --     where 
+    --         answer :: RoseTree b
+    --         answer = (List.<*>) (fs <*>) xs    
+            
+-- How to return [a] if emptyList of functions is passed in
+-- listApply :: [(a -> b)] -> [a] -> [b]
+-- listApply [] _  = []
+-- listApply _  [] = []
+-- listApply fs xs = [ f x | f <- fs, x <- xs] 
 
 -- What about when a player passed?
 -- prevMove :: GameState -> Location
