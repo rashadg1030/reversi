a88d2abeec906fada492ce04bda674d60d044e13
rashadg1030 <rashad.sasaki@gmail.com>
2019-05-03 00:52:21 -0700

    Changed file structure.

---
+++ src/Reversi/Types.hs
@@ -0,0 +1,21 @@
+module Reversi.Types where
+
+import Data.Map.Strict (Map)
+
+data Disc = Black | White
+    deriving (Show, Eq, Ord) --Ord??
+
+data Final = Win Disc | Tie 
+  deriving (Show, Eq)
+  
+type Cell = Maybe Disc
+
+type Location = (Int, Int)
+
+data Move = Pass | Move Location
+  deriving (Show, Eq, Ord) -- Ord??
+
+type Board = Map Location Disc
+
+data GameState = GameState { getDisc :: Disc, getBoard :: Board, getMove :: Move, getFrames :: [GameState]  }
+  deriving (Show, Eq, Ord)
